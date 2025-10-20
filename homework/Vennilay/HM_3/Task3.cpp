51e1cd47bb361e0f2a4906fde2a0475038ebff2a
Vennilay <vennilay@outlook.com>
2025-10-20 10:54:52 +0300

    Added Task3.cpp and Task4.cpp

--- homework/Vennilay/HM_3/Task3.cpp
+++ homework/Vennilay/HM_3/Task3.cpp
@@ -1,22 +1,70 @@
 #include <iostream>
-#include <fstream>
+#include <string>
+
+
+void bubbleSort(std::string& str) {
+    int n = str.length();
+
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = 0; j < n - i - 1; j++) {
+            if (str[j] > str[j + 1]) {
+                char temp = str[j];
+                str[j] = str[j + 1];
+                str[j + 1] = temp;
+            }
+        }
+    }
+}
 
 int main() {
-    std::string filename = "files/Vennilay/HM_3/task3.txt";
-    std::fstream file(filename);
+    std::string line;
+    std::string s = "";
+    int i = 0;
 
-    if (!file.is_open()) {
-        std::cout << "Ошибка открытия файла!" << std::endl;
+    std::cout << "Введите строку (буквы латиницы или кириллицы): ";
+    getline(std::cin, line);
+
+    while ((s.length() < 30) && (i < line.length()))
+    {
+        if (((line[i] > 64) && (line[i] <= 90)) ||
+            ((line[i] > 96) && (line[i] <= 122)) ||
+            (line[i] < 0))
+        {
+            s = s + line[i];
+        }
+        i++;
+    }
+
+    if ((s.length() == 30) && (i < line.length())) {
+        bool hasInvalidChars = false;
+
+        for (int j = i; j < line.length(); j++) {
+            if (!(((line[j] > 64) && (line[j] <= 90)) ||
+                  ((line[j] > 96) && (line[j] <= 122)) ||
+                  (line[j] < 0)))
+            {
+                hasInvalidChars = true;
+                break;
+            }
+        }
+
+        if (hasInvalidChars) {
+            std::cout << "Ошибка: после 30-й буквы обнаружен посторонний символ!" << std::endl;
+            return 1;
+        }
+    }
+
+    if (s.empty()) {
+        std::cout << "Ошибка: строка не содержит букв!" << std::endl;
         return 1;
     }
 
-    std::cout << "Открываю: " << filename << std::endl;
-    std::string line;
+    std::cout << "\nИсходная строка (только буквы): " << s << std::endl;
+    std::cout << "Количество букв: " << s.length() << std::endl;
 
-    while (std::getline(file, line)) {
-        std::cout << line << std::endl;
-    }
-    file.close();
+    bubbleSort(s);
+
+    std::cout << "Отсортированная строка: " << s << std::endl;
 
     return 0;
 }
