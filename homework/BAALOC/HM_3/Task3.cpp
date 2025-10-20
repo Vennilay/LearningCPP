f7cd5f6cfc01db7c50faa0f53f35db6d99fcdd28
Makar Leonardov <makarleonardov@gmail.com>
2025-10-20 10:52:15 +0300

    Added HW_3 BAALOC

--- homework/BAALOC/HM_3/Task3.cpp
+++ homework/BAALOC/HM_3/Task3.cpp
@@ -1,12 +1,23 @@
 #include <iostream>
 
 int main() {
-    const std::string alph = "abcdefghijklmnopqrstuvwxyz";
+    std::string alph = "abcdefghijklmnopqrstuvwxyz";
     std::string line = "qweasdzxcvbnmrewqplmoknijuhyt";
+    std::string newLine;
+    int last_index = 0;
 
-    for (int i = 0; i < line.size(); i++) {
-        std::cout << line[i];
+    while (line.data()) {
+        for (char sym : line) {
+            if (alph.find(sym) == last_index) {
+                newLine += sym;
+                last_index += 1;
+                std::cout << newLine << std::endl;
+                line.erase(sym, 1);
+            }
+        }
     }
 
+
+    std::cout << newLine << std::endl;
     return 0;
 }
