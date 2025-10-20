5163b28b1f3334f3fb5cbff7d203a03a53f74a81
vondre <artemf1000@gmail.com>
2025-10-20 10:06:47 +0300

    zalil domashku 3.1

--- homework/Vondre/HM_3/vuz3.1.cpp
+++ homework/Vondre/HM_3/vuz3.1.cpp
@@ -5,10 +5,8 @@
 
 int main() {
     std::ifstream file("./files/Vondre/HM_3/task1.txt");
-
     std::string content;
     content.assign((std::istreambuf_iterator<char>(file)),(std::istreambuf_iterator<char>()));
-
     std::cout << "Содержимое файла:\n" << content << std::endl;
     file.close();
 }
