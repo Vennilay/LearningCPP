e2a88dc36705a1706166e5baeb1934350a754ec5
Makar Leonardov <makarleonardov@gmail.com>
2025-10-20 10:52:15 +0300

    Added HW_3 BAALOC

--- homework/BAALOC/HM_3/Task5.cpp
+++ homework/BAALOC/HM_3/Task5.cpp
@@ -1,4 +1,26 @@
 #include <iostream>
 
+bool isPrime(int n) {
+    if (n == 2) {
+        return true;
+    }
+    for (int i = 2; i <= std::sqrt(n) + 1; i++) {
+        if (n % i == 0) {
+            return false;
+        }
+    }
+    return true;
+}
+
 int main() {
+    int n = 0;
+    std::cout << "Введите число: ";
+    std::cin >> n;
+
+    for (int i = 2; i <= n; i++) {
+        if (isPrime(i)) {
+            std::cout << i << std::endl;
+        }
+    }
+    return 0;
 }
