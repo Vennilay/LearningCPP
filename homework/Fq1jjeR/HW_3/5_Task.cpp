ffc54630d1a9a59419af180e022de242878eada3
Fq1jjeR <nikitanozdrin00@gmail.com>
2025-10-22 22:42:11 +0300

    update

--- homework/Fq1jjeR/HW_3/5_Task.cpp
+++ homework/Fq1jjeR/HW_3/5_Task.cpp
@@ -2,13 +2,33 @@
 #include <iostream>
 #include <string>
 #include <Windows.h>
+#include <vector>
 
 using namespace std;
 
 int main() {
-    SetConsoleCP(CP_UTF8);
-    SetConsoleOutputCP(CP_UTF8);
+    int N;
+    cout << "Введите натуральное число:";
+    cin >> N;
 
+    vector<bool> is_prime(N + 1, true);
+    is_prime[0] = is_prime[1] = false; // 0 и 1 не являются простыми
 
+    // Решето Эратосфена
+    for (int i = 2; i * i <= N; i++) {
+        if (is_prime[i]) {
+            for (int j = i * i; j <= N; j += i) {
+                is_prime[j] = false;
+            }
+        }
+    }
 
+    // Собираем и выводим простые числа
+    for (int i = 2; i <= N; i++) {
+        if (is_prime[i]) {
+            cout << i << " ";
+        }
+    }
+
+    return 0;
 }
