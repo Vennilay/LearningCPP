d357e9ad5b80de8701736ac2cc51b3c370fed448
Fq1jjeR <nikitanozdrin00@gmail.com>
2025-10-24 21:16:07 +0300

    update

---
+++ homework/Fq1jjeR/HW_4/2_Task.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <Windows.h>
+
+using namespace std;
+
+int main()
+{
+    SetConsoleCP(CP_UTF8);
+    SetConsoleOutputCP(CP_UTF8);
+   /*
+
+     Купил мужик шляпу, а она ему как раз.
+        Купил ещё одну, а она ему как два
+
+
+    шёл медведь по городу, видит: ларёк. ну он открыл бизнес и прогорел.
+
+    */
+}
