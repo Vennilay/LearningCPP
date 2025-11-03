4639e52dbb2d689344046a8fa6d849b2ed462cac
Fq1jjeR <nikitanozdrin00@gmail.com>
2025-11-03 09:14:36 +0300

    update

--- homework/Fq1jjeR/HW_4/1_Task.cpp
+++ homework/Fq1jjeR/HW_4/1_Task.cpp
@@ -1,16 +1,43 @@
 #include <iostream>
-#include <Windows.h>
+#include <iomanip>
+#include <windows.h>
 
 using namespace std;
+
+
+double rectangleArea(double width, double heightRectangle) { return width * heightRectangle; }
+double triangleArea(double base, double heightTriangle) { return (base * heightTriangle) / 2; }
+double circleArea(double radius, double PI) { return PI * (radius * radius) ; }
+
 
 int main()
 {
     SetConsoleCP(CP_UTF8);
     SetConsoleOutputCP(CP_UTF8);
 
-    /*
 
-        Стоят два барана в поле. Один бээээ. Второй бля тож самое хотел сказать.
+    cout << "Прямоугольник" << endl;
+    double width, heightRectangle;
 
-     */
+    cout << "Введите ширину: "; cin >> width;
+    cout << "Введите высоту: "; cin >> heightRectangle;
+    cout << "Площадь: " << fixed << setprecision(2) << rectangleArea(width, heightRectangle) << endl;
+
+
+    cout << "Треугольник" << endl;
+    double base, heightTriangle;
+
+    cout << "Введите основание: "; cin >> base;
+    cout << "Введите высоту: "; cin >> heightTriangle;
+    cout << "Площадь: " << fixed << setprecision(2) << triangleArea(base, heightTriangle) << endl;
+
+
+    cout << "Круг" << endl;
+    const double PI = 3.141592653589793;
+    double radius = 5;
+
+    cout << "Введите радиус: "; cin >> radius;
+    cout << "Площадь: " << fixed << setprecision(2) << circleArea(radius, PI) << endl;
+
+    return 0;
 }
