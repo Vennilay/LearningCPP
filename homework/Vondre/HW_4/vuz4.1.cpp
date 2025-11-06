fbf5797542fb1fc35814a43d9b9b676b8ca80098
vondre <artemf1000@gmail.com>
2025-11-06 10:56:03 +0300

    zalil domashku 4.1

---
+++ homework/Vondre/HW_4/vuz4.1.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+
+void pryamougolnik() {
+    int shirina, height;
+    std::cout<<"Введите ширину ";
+    std::cin>>shirina;
+    std::cout<<"Введите высоту ";
+    std::cin>>height;
+    if (height <= 0 or shirina <= 0) std::cout<<"Введено некорректное значение";
+    else std::cout<<"Площадь прямоугольника равна "<<shirina*height<< std::endl;
+}
+
+void treugolnik() {
+    int osnov, height;
+    std::cout<<"Введите длину основания ";
+    std::cin>>osnov;
+    std::cout<<"Введите высоту ";
+    std::cin>>height;
+    if (height <= 0 or osnov <= 0) std::cout<<"Введено некорректное значение";
+    else std::cout<<"Площадь треугольника равна "<<osnov * height * 0.5<< std::endl;
+}
+
+void krug() {
+    int radius;
+    std::cout<<"Введите рудиус ";
+    std::cin>>radius;
+    if (radius <= 0) std::cout<<"Введено некорректное значение";
+    else std::cout<<"Площадь треугольника равна "<<radius*radius*3.1415926<< std::endl;
+}
+
+int main(){
+    char again = 'y';
+    while (again == 'y') {
+        std::cout<<R"(Выберите, площадь какой фигуры вы хотите узнать?
+1 - Прямоугольник
+2 - Треугольник
+3 - Круг)"<<std::endl;
+        int vibor;
+        std::cin>>vibor;
+        switch (vibor) {
+            case 1:
+                pryamougolnik();
+                break;
+            case 2:
+                treugolnik();
+                break;
+            case 3:
+                krug();
+                break;
+        }
+
+        std::cout<<"Хотите узнать площадь другой фигуры? y/n"<< std::endl;
+        std::cin>>again;
+    }
+}
