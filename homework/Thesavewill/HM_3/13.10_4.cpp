6d8369fa024a76b27b55a75540c395c5eb5fed95
Thesavewill <denes3040@gmail.com>
2025-11-10 12:53:21 +0300

    update all HW

---
+++ homework/Thesavewill/HM_3/13.10_4.cpp
@@ -0,0 +1,45 @@
+﻿#include <iostream>
+#include <windows.h>
+using namespace std;
+int main()
+{
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+    int a, b, f;
+    cout << "Выберите способ (1 - делением, 2 - вычитанием): "; cin >> f;
+    if (f == 1) {
+        cout << "Выбран Алгоритм Евклида через деление\n";
+        cout << "Введите первое число: "; cin >> a;
+        cout << "Введите второе число: "; cin >> b;
+            while (a != 0 and b != 0) {
+                if (a > b) {
+                    a = a % b;
+                }
+                else {
+                    b = b % a;
+                }
+            }
+            cout << a + b;
+    }
+
+    else if (f == 2) {
+        cout << "Выбран Алгоритм Евклида через вычитание\n";
+        cout << "Введите первое число: "; cin >> a;
+        cout << "Введите второе число: "; cin >> b;
+        if (a != 0 and b != 0) {
+            while (a != b) {
+                if (a > b) {
+                    a = a - b;
+                }
+                else {
+                    b = b - a;
+                }
+            }
+            cout << a;
+        }
+        else {
+            cout << "Первое и второе число не должны быть равно нулю!";
+        }
+    }
+    return 0;
+}
