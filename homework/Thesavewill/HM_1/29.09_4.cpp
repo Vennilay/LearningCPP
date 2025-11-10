7a3797b384cd584b91ff7fd3009bf054a5ce12ba
Thesavewill <denes3040@gmail.com>
2025-11-10 12:41:28 +0300

    add HW_1_2_3_4_5

---
+++ homework/Thesavewill/HM_1/29.09_4.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+#include <windows.h>
+#include <cmath>
+using namespace std;
+int main()
+{
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+    double y, x = -4;
+    while (x <= 4) {
+        if (x == 1) {
+            cout << "При x = 1, функция в этой точке не существует" << endl;
+        }
+        else {
+            y = (pow(x, 2) - 2 * x + 2) / (x - 1);
+            cout << "При x = " << x << ", y = " << y << endl;
+        }
+        x = x + 0.5;
+    }
+	return 0;
+}
