6e684df963b9da162eb42a2c1de47cd79f4ffefb
Thesavewill <denes3040@gmail.com>
2025-11-13 12:21:36 +0300

    add Movment Cursor Console

---
+++ files/Thesavewill/MovmentCursor.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+#include <cmath>
+#include <windows.h>
+using namespace std;
+int main()
+{
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+    const char spinner[] = "|/-\\";
+
+    for (int i = 0; i < 30; ++i) {                                 // \r переносит курсор консоли в начало и пишет заново строку заменяя символы,
+        cout << "\r" << spinner[i % 4] << " Загрузка...";          // если недостаточно места для новой строки.
+        Sleep(100);                                                // Sleep(x) - заставляет систему ждать X мс.
+    }
+    cout << "\rГотово, вы натурал!";                               // Копиум
+}
