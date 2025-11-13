5e63a0aab850ee98396e6471607426f1227e35f4
vondre <artemf1000@gmail.com>
2025-11-13 07:59:46 +0300

    4.2 добавил внизу 13 полосу 4.3 залил

--- homework/Vondre/HW_4/vuz4.2.cpp
+++ homework/Vondre/HW_4/vuz4.2.cpp
@@ -2,11 +2,13 @@
 
 int main() {
     for (int i = 0; i<3; i++) {
-        std::cout << "\033[44m" <<" * * * * * * * * "<< "\033[41m" << "\t\t\t\t\t\t\t\t\t\t"<< "\033[0m" << std::endl;
-        std::cout << "\033[44m" << " * * * * * * * * "<<"\33[107m" << "\t\t\t\t\t\t\t\t\t\t" << "\033[0m" << std::endl;
+        std::cout << "\033[44m" <<" *  *  *  *  *  *  *  *  "<< "\033[41m" << "\t\t\t\t\t\t\t\t\t\t"<< "\033[0m" << std::endl;
+        std::cout << "\033[44m" << " *  *  *  *  *  *  *  *  "<<"\33[107m" << "\t\t\t\t\t\t\t\t\t\t" << "\033[0m" << std::endl;
     }
     for (int i = 0; i<3; i++) {
-        std::cout << "\033[41m" << "\t\t\t\t\t\t\t\t\t\t\t\t\t\t"<< "\033[0m" << std::endl;
-        std::cout <<"\33[107m" << "\t\t\t\t\t\t\t\t\t\t\t\t\t\t" << "\033[0m" << std::endl;
+        std::cout << "\033[41m" << "\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t"<< "\033[0m" << std::endl;
+        std::cout <<"\33[107m" << "\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t" << "\033[0m" << std::endl;
     }
+    std::cout << "\033[41m" << "\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t"<< "\033[0m" << std::endl;
+
 }
