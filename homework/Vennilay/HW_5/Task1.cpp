e174985e8ed5259216004f5dd92c30f07fd52502
Vennilay <229026665+Vennilay@users.noreply.github.com>
2025-11-24 09:54:56 +0300

    Task1.cpp fixed in HW_5

--- homework/Vennilay/HW_5/Task1.cpp
+++ homework/Vennilay/HW_5/Task1.cpp
@@ -3,61 +3,46 @@
 
 int getValue(const char c) {
     switch (c) {
-    case 'I': return 1;
-    case 'V': return 5;
-    case 'X': return 10;
-    case 'L': return 50;
-    case 'C': return 100;
-    case 'D': return 500;
-    case 'M': return 1000;
-    default: return 0;
+        case 'I': return 1;
+        case 'V': return 5;
+        case 'X': return 10;
+        case 'L': return 50;
+        case 'C': return 100;
+        case 'D': return 500;
+        case 'M': return 1000;
+        default: return 0;
     }
-}
-
-bool isValidRoman(const std::string& roman) {
-    if (roman.empty()) return false;
-
-    for (const char c : roman) {
-        if (getValue(c) == 0) return false;
-    }
-
-    int repeatCount = 1;
-    for (size_t i = 1; i < roman.size(); i++) {
-        if (roman[i] == roman[i - 1]) {
-            repeatCount++;
-            if (roman[i] == 'V' || roman[i] == 'L' || roman[i] == 'D') return false;
-            if (repeatCount > 3) return false;
-        } else {
-            repeatCount = 1;
-        }
-    }
-
-    for (size_t i = 0; i + 1 < roman.size(); i++) {
-        int current = getValue(roman[i]);
-        int next = getValue(roman[i + 1]);
-        if (current < next) {
-            if (!((roman[i] == 'I' && (roman[i + 1] == 'V' || roman[i + 1] == 'X')) ||
-                  (roman[i] == 'X' && (roman[i + 1] == 'L' || roman[i + 1] == 'C')) ||
-                  (roman[i] == 'C' && (roman[i + 1] == 'D' || roman[i + 1] == 'M')))) {
-                return false;
-                  }
-        }
-    }
-
-    return true;
 }
 
 int decodeRoman(const std::string& roman) {
     int result = 0;
-    for (size_t i = 0; i < roman.size(); ++i) {
-        int current = getValue(roman[i]);
-        if (i + 1 < roman.size() && current < getValue(roman[i + 1])) {
-            result -= current;
+    const size_t length = roman.length();
+
+    for (size_t i = 0; i < length; i++) {
+        const int current = getValue(roman[i]);
+        if (i + 1 < length && current < getValue(roman[i + 1])) {
+            result = result - current;
         } else {
-            result += current;
+            result = result + current;
         }
     }
     return result;
+}
+
+std::string encodeRoman(int num) {
+    if (num <= 0) return "";
+
+    const int vals[] = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
+    const std::string strs[] = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
+
+    std::string res;
+    for (int i = 0; i < 13; i++) {
+        while (num >= vals[i]) {
+            res += strs[i];
+            num -= vals[i];
+        }
+    }
+    return res;
 }
 
 int main() {
@@ -65,12 +50,22 @@ int main() {
     std::cout << "Введите римское число: ";
     std::cin >> roman;
 
-    if (!isValidRoman(roman)) {
-        std::cout << "Ошибка: некорректное римское число!" << std::endl;
-        return 1;
+    for (size_t i = 0; i < roman.length(); i++) {
+        if (getValue(roman[i]) == 0) {
+            std::cout << "Ошибка: посторонние символы!" << std::endl;
+            return 1;
+        }
     }
 
-    std::cout << roman << " = " << decodeRoman(roman) << std::endl;
+    const int number = decodeRoman(roman);
+
+    const std::string check = encodeRoman(number);
+
+    if (check == roman) {
+        std::cout << roman << " = " << number << std::endl;
+    } else {
+        std::cout << "Ошибка: нарушение правил записи" << std::endl;
+    }
 
     return 0;
 }
