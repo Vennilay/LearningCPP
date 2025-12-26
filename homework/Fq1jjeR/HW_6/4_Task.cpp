a20410d4d9d888fb047fe7175b5a85f35e33405b
nikit <nikitanozdrin00@gmail.com>
2025-12-26 19:40:07 +0300

    HW_6 Prerelease

---
+++ homework/Fq1jjeR/HW_6/4_Task.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <set>
+
+using namespace std;
+
+int main() {
+    int n; // Количество свободных мест
+    if (!(cin >> n) || cin.peek() == '.' || cin.peek() == ',') {
+        return -1;
+    }
+
+    set<int> free_seats;
+    for (int i = 0; i < n; ++i) {
+        int seat_num; //
+        cin >> seat_num;
+        free_seats.insert(seat_num);
+    }
+
+    int max_c = 0; // max_consecutive_free_compartments
+    int cur_c = 0; // current_consecutive_free_compartments
+
+    for (int k = 1; k <= 9; ++k) {
+        int main_start = 4 * (k - 1) + 1;
+        int main_end = 4 * k;
+        int side1 = 54 - 2 * (k - 1);
+        int side2 = 53 - 2 * (k - 1);
+
+        bool is_c_free = true; // is_compartment_free
+        for (int s = main_start; s <= main_end; ++s) {
+            if (free_seats.find(s) == free_seats.end()) {
+                is_c_free = false;
+                break;
+            }
+        }
+
+        if (is_c_free) {
+            if (free_seats.find(side1) == free_seats.end() || free_seats.find(side2) == free_seats.end())
+                is_c_free = false;
+        }
+
+        if (is_c_free)
+            cur_c++;
+
+        else {
+            max_c = (cur_c > max_c) ? cur_c : max_c;
+            cur_c = 0;
+        }
+    }
+
+    max_c = (cur_c > max_c) ? cur_c : max_c;
+
+    cout << max_c << endl;
+}
