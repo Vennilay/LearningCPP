1b5e424a6c746f8bd473b5aa408bd97ba2ea7d33
nikit <nikitanozdrin00@gmail.com>
2025-12-29 11:08:06 +0300

    HW_6 Release

--- Semester_1/Fq1jjeR/HW_6/3_Task.cpp
+++ Semester_1/Fq1jjeR/HW_6/3_Task.cpp
@@ -11,8 +11,8 @@ int main() {
     cout << "Введите количевство M (столбцов): ";
     cin >> M;
 
-    int count_N = N * (N + 1) / 2;
-    int count_M = M * (M + 1) / 2;
+    long long count_N = N * (N + 1) / 2;
+    long long count_M = M * (M + 1) / 2;
 
     cout << "Количество прямоугольников: " << count_N * count_M << endl;
 
