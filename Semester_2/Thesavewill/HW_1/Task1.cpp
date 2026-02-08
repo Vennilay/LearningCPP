29d3b85bd524d0583567e1f1ae1952e668887f61
Thesavewill <denes3040@gmail.com>
2026-02-08 19:33:46 +0300

    Test fa pepe

--- Semester_2/Thesavewill/HW_1/Task1.cpp
+++ Semester_2/Thesavewill/HW_1/Task1.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 
 int main() {
-    std::cout << "Test file for HW_1 Task1 | Student: $student" << std::endl;
+    std::cout << "Test file for HW_1 Task1 | Student: $studentasdhfg ausydfgahsdgf iwрвыпагнфыпваоцруг" << std::endl;
     return 0;
 }
