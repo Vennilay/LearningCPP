5825cb8d81441044d0045ce22c8e7d9fe78820b6
Vondre <artemf1010@gmail.com>
2026-03-04 00:35:20 +0300

    сделал директорию для сиаода и закинул в нее задание с первой функцией

--- Semester_2/Vondre/siaod/1.cpp
+++ Semester_2/Vondre/siaod/1.cpp
@@ -97,5 +97,4 @@ int main() {
     auto end = chrono::high_resolution_clock::now();
     auto duration = chrono::duration_cast<chrono::milliseconds>(end - start);
     cout << "Время выполнения: " << duration.count() << " мс\n";
-
 }
