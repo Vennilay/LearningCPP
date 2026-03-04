b934d93fea9b6f13cf5814bd66acd7d0f49e20b4
vondre <artemf1010@gmail.com>
2026-03-04 21:54:38 +0300

    автотест

--- Semester_2/Vondre/siaod/1.cpp
+++ Semester_2/Vondre/siaod/1.cpp
@@ -28,7 +28,7 @@ void delFirstMetod(char*x, int &n, char key, int &comparisons, int &movements) {
     comparisons = 0;
     movements = 0;
     int i = 0;
-    while (i <= n) {
+    while (i <= n - 1) {
         comparisons++;
         if (x[i] == key) {
             for (int j = i; j <= n-1; j++) {
@@ -43,36 +43,7 @@ void delFirstMetod(char*x, int &n, char key, int &comparisons, int &movements) {
     }
 }
 
-int main() {
-
-    int comparisons;
-    int movements;
-
-    string vvod;
-    int n;
-    char key;
-
-    int vibor;
-
-    cout<<"Ввод строки вручную или тестирование скорости:"<<endl<<"1) Вручную"<<endl<<"2) Тест"<<endl;
-    cin>>vibor;
-
-    if (vibor == 1) {
-        cout<<"Введите строку: ";
-        getline(cin,vvod);
-        n = vvod.size();
-    }
-
-    if (vibor == 2) {
-        cout<<"Введите длину строки: ";
-        cin>>n;
-        vvod = generateRandomString(n);
-        cout<<"Cгенерированная строка: "<<vvod<<endl;
-    }
-    else {
-        return 0;
-    }
-
+void activity(char &key, int n, string &vvod, int &comparisons, int &movements){
     cout<<"Введите символ который нужно удалить: ";
     cin>>key;
 
@@ -90,6 +61,47 @@ int main() {
     cout<<"Новая длина: "<<n<<endl;
     delete[] x;
 
-    auto duration = chrono::duration_cast<chrono::milliseconds>(end - start);
+    auto duration = chrono::duration<double, milli>(end - start);
     cout << "Время выполнения: " << duration.count() << " мс\n";
+    cout<<"Сп: "<<comparisons<<endl;
+    cout<<"Мп: "<<movements<<endl;
+    cout<<"Тп: "<<comparisons + movements<<endl;
+}
+
+int main() {
+
+    int test[7] = {100, 200, 500, 1000, 2000, 5000, 10000};
+
+    int comparisons;
+    int movements;
+
+    string vvod;
+    int n;
+    char key;
+
+    int vibor;
+
+    cout<<"Ввод строки вручную или тестирование скорости:"<<endl<<"1) Вручную"<<endl<<"2) Тест"<<endl;
+    cin>>vibor;
+    cin.ignore();
+
+    if (vibor == 1) {
+        cout<<"Введите строку: "<<endl;
+        getline(cin, vvod);
+        n = vvod.size();
+        activity(key, n, vvod, comparisons, movements);
+    }
+
+    if (vibor == 2) {
+        for (int n : test)
+        {
+            vvod = generateRandomString(n);
+            cout<<"Cгенерированная строка: "<<vvod<<endl;
+
+            activity(key, n, vvod, comparisons, movements);
+        }
+    }
+    else {
+        return 0;
+    }
 }
