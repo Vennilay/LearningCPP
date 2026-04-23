e6aef22204ecda79049de3d9f91d1bd049c200bd
nikit <nikitanozdrin00@gmail.com>
2026-04-23 14:53:06 +0300

    OOP: PR_3 -> add 3.1

--- Semester_2/Fq1jjeR/OOP/PR_3/3_1/main.cpp
+++ Semester_2/Fq1jjeR/OOP/PR_3/3_1/main.cpp
@@ -1,7 +1,35 @@
-#include  <iostream>
+#include <iostream>
+#include <string>
+#include "Items.h"
+
 using namespace std;
+
 int main()
 {
-    cout << "Hello World!";
-    return 0;
+    string n;
+    int d, w;
+    cin >> n >> d >> w;
+    Items obj(n, d);
+
+    repairItem(obj);
+    cout << "After repair: " << obj.getName()
+        << " durability = " << obj.getDurability()
+        << endl;
+
+    damageItem(obj);
+    cout << "After damage: " << obj.getName()
+        << " durability = " << obj.getDurability()
+        << endl;
+
+    Items* itemPtr = createBrokenItem(w);
+    if (itemPtr == nullptr)
+        cout << "Broken pointer is nullptr";
+
+    else
+        cout << "Broken item: " << itemPtr -> getName()
+        << "durability = " << itemPtr -> getDurability()
+        << endl;
+
+    delete itemPtr;
+    return(0);
 }
