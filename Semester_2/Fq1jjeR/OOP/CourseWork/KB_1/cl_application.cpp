cfd43ae84fccd442f8c4a7109eb26c66d03eef5f
nikit <nikitanozdrin00@gmail.com>
2026-04-27 09:39:02 +0300

    OPP: КВ_1 -> add files

--- Semester_2/Fq1jjeR/OOP/CourseWork/KB_1/cl_application.cpp
+++ Semester_2/Fq1jjeR/OOP/CourseWork/KB_1/cl_application.cpp
@@ -14,45 +14,31 @@ void cl_application::build_tree_objects() {
     cin >> s_head;
     this->set_object_name(s_head);
 
+    p_head = this;
     bool use_first_derived = true;
 
     while(true) {
         cin >> s_head >> s_sub;
+        if(s_head == s_sub) break;
 
-        if(s_head == s_sub)
-            break;
-
-        cl_base* p_found_head = nullptr;
-        vector<cl_base*> bfs_queue;
-        bfs_queue.push_back(this);
-        int bfs_idx = 0;
-
-        while(bfs_idx < (int)bfs_queue.size()) {
-            cl_base* current = bfs_queue[bfs_idx++];
-            if(current->get_object_name() == s_head) {
-                p_found_head = current;
-                break;
-            }
-            for(int i = 0; i < current->get_count_subordinate_objects(); ++i) {
-                bfs_queue.push_back(current->find_subordinate_by_index(i + 1));
-            }
+        cl_base* p_found_head = p_head->find_subordinate_by_name(s_head);
+        if(p_found_head != nullptr && p_head->get_count_subordinate_objects() > 0 &&
+           p_found_head == p_head->find_subordinate_by_index(p_head->get_count_subordinate_objects())) {
+            p_head = p_found_head;
         }
 
-        if(p_found_head == nullptr) {
-            continue;
-        }
-
-        if(p_found_head->find_subordinate_by_name(s_sub) != nullptr) {
+        if((p_head != this && p_head->get_object_name() != s_head) ||
+           p_head->find_subordinate_by_name(s_head) != nullptr ||
+           p_head->find_subordinate_by_name(s_sub) != nullptr) {
             continue;
         }
 
         if(use_first_derived) {
-            p_sub = new cl_derived(p_found_head, s_sub);
+            p_sub = new cl_derived(p_head, s_sub);
         } else {
-            p_sub = new cl_derived(p_found_head, s_sub);
+            p_sub = new cl_derived(p_head, s_sub);
         }
         use_first_derived = !use_first_derived;
-
     }
 }
 
@@ -62,69 +48,63 @@ int cl_application::exec_app() {
     vector<cl_base*> queue;
     queue.push_back(this);
     int idx = 0;
-    while (idx < (int)queue.size()) {
+    while(idx < (int)queue.size()) {
         cl_base* current = queue[idx++];
-        if (current->get_count_subordinate_objects() > 0) {
+        if(current->get_count_subordinate_objects() > 0) {
             cout << current->get_object_name();
-            for (int i = 0; i < current->get_count_subordinate_objects(); ++i) {
+            for(int i = 0; i < current->get_count_subordinate_objects(); ++i) {
                 cl_base* child = current->find_subordinate_by_index(i + 1);
                 cout << "  " << child->get_object_name();
             }
             cout << endl;
         }
-        for (int i = 0; i < current->get_count_subordinate_objects(); ++i) {
+        for(int i = 0; i < current->get_count_subordinate_objects(); ++i) {
             queue.push_back(current->find_subordinate_by_index(i + 1));
         }
     }
+    cout << endl;
 
     int level, index;
     string new_name;
-    while (cin >> level) {
-        if (level == 0) break;
+    while(cin >> level) {
+        if(level == 0) break;
         cin >> index >> new_name;
 
-        vector<cl_base*> nodes_at_level;
-        vector<cl_base*> bfs_queue;
-        vector<int> levels;
-        bfs_queue.push_back(this);
-        levels.push_back(1);
-        int bfs_idx = 0;
-
-        while (bfs_idx < (int)bfs_queue.size()) {
-            cl_base* current = bfs_queue[bfs_idx];
-            int current_level = levels[bfs_idx++];
-
-            if (current_level == level) {
-                nodes_at_level.push_back(current);
-            } else if (current_level < level) {
-                for (int i = 0; i < current->get_count_subordinate_objects(); ++i) {
-                    bfs_queue.push_back(current->find_subordinate_by_index(i + 1));
-                    levels.push_back(current_level + 1);
-                }
-            }
+        int depth = 1;
+        cl_base* p_head = this;
+        if(level == 1 && index == 1) {
+            this->set_object_name(new_name);
+            continue;
         }
-
-        if (index >= 1 && index <= (int)nodes_at_level.size()) {
-            nodes_at_level[index - 1]->set_object_name(new_name);
+        while(true) {
+            if(depth < level - 1) {
+                if(p_head->get_count_subordinate_objects() > 0)
+                    p_head = p_head->find_subordinate_by_index(p_head->get_count_subordinate_objects());
+                depth++;
+                continue;
+            }
+            if(p_head != nullptr && p_head->find_subordinate_by_index(index) != nullptr) {
+                p_head->find_subordinate_by_index(index)->set_object_name(new_name);
+            }
+            break;
         }
     }
 
     cout << this->get_object_name() << endl;
-
     queue.clear();
     queue.push_back(this);
     idx = 0;
-    while (idx < (int)queue.size()) {
+    while(idx < (int)queue.size()) {
         cl_base* current = queue[idx++];
-        if (current->get_count_subordinate_objects() > 0) {
+        if(current->get_count_subordinate_objects() > 0) {
             cout << current->get_object_name();
-            for (int i = 0; i < current->get_count_subordinate_objects(); ++i) {
+            for(int i = 0; i < current->get_count_subordinate_objects(); ++i) {
                 cl_base* child = current->find_subordinate_by_index(i + 1);
                 cout << "  " << child->get_object_name();
             }
             cout << endl;
         }
-        for (int i = 0; i < current->get_count_subordinate_objects(); ++i) {
+        for(int i = 0; i < current->get_count_subordinate_objects(); ++i) {
             queue.push_back(current->find_subordinate_by_index(i + 1));
         }
     }
